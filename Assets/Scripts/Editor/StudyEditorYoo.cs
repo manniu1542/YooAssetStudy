ce517aa65cd6d5b565145636a473e64bbc84dafa
zxw <819400842@qq.com>
2024-01-03 18:00:26 +0800

    同步加载yoo资源 ，不能加载。 需要找到yoo加载 的正确方式

--- Assets/Scripts/Editor/StudyEditorYoo.cs
+++ Assets/Scripts/Editor/StudyEditorYoo.cs
@@ -1,7 +1,45 @@
 //自定义扩展范例
 using System.IO;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
 using YooAsset.Editor;
+using BuildReport = UnityEditor.Build.Reporting.BuildReport;
+using BuildResult = UnityEditor.Build.Reporting.BuildResult;
 
+public class AA
+{
+    [MenuItem("Tools/GG")]
+    public static void GG()
+    {
+     
+        //BuildPipeline.BuildAssetBundles(Application.dataPath, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows64);
+        AssetDatabase.Refresh();
+
+
+        // 设置构建的平台和目标路径
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Bundle/Scene/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "Builds/MyGame.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+
+        // 使用自定义的构建管线
+        BuildReport report = UnityEditor.BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        // 处理构建报告，获取构建信息
+        BuildSummary summary = report.summary;
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+        else
+        {
+            Debug.LogError("Build failed");
+        }
+        AssetDatabase.Refresh();
+
+    }
+}
 
 public class TestPakeage: IPackRule
 {
--- Assets/Scripts/TestYoo.cs
+++ Assets/Scripts/TestYoo.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using YooAsset;
 
-public class TestYoo:MonoBehaviour
+public class TestYoo : MonoBehaviour
 {
     private void Start()
     {
+        // 初始化资源系统
+        YooAssets.Initialize();
+
+        // 创建默认的资源包
+        var package = YooAssets.CreatePackage("DefaultPackage");
+
+        // 设置该资源包为默认的资源包，可以使用YooAssets相关加载接口加载该资源包内容。
+        YooAssets.SetDefaultPackage(package);
+
+        StartCoroutine(InitializeYooAsset(package, () =>
+        {
+            var go = YooAssets.LoadAssetSync<GameObject>("GameObject");
+            GameObject.Instantiate(go.GetAssetObject<GameObject>());
+
+        }));
+
+
+     
+        Debug.Log("初始化完成！");
+    }
+    private IEnumerator InitializeYooAsset(ResourcePackage package,Action act)
+    {
+        var initParameters = new OfflinePlayModeParameters();
+        yield return package.InitializeAsync(initParameters);
+
+
+        act?.Invoke();
         
 
-
     }
-
 
 }
